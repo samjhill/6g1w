013675714af9b2008b955a95928ff3659daa7dc9
Sam Hill <samuhill@gmail.com>
2013-12-04 23:07:43 -0500

    js shenanigans

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 function getGallery(search){
     console.log('getGallery called');
-    $.getJSON({
-        url: "php/proxy.php?search=" + search,
-        //url: "http://www.cssmania.com/api/api.php?search=" + search,
-        success: function (result) {
-            $("#gallery").append(result);
-        }
-    });
+    $.getJSON( "php/proxy.php", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+       
+        $( "<ul/>", {
+          "class": "my-new-list",
+          html: items.join( "" )
+        }).appendTo( "body" );
+      });
 }
 
