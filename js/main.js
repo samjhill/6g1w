70beb108bdf77cd555ba91cdf54096827ee3d3cd
Sam Hill <samuhill@gmail.com>
2013-12-05 01:24:49 -0500

    its confusing

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function parseXml(xml){
             var title = $(this).find("title").text();
 	    var link =  $(this).find("link").text();
             $('#gallery').append('<h3>' + title + '</h3>');
-            $('#gallery').append('<img src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ&url=' + link + '"></img>');
+            $('#gallery').append('<img src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=' + link + '"></img>');
 	    $('#gallery').append('<a href="' + link + '">' + link + '</a></div>');
     });
     
