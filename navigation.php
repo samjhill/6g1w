0fbf711ad8b0fb56be22b307c65a379d9ca1d435
LiLiu <lxl3889@g.rit.edu>
2013-12-04 21:21:20 -0500

    index nav change

---
+++ navigation.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+	<div class="navbar-inner">
+	  <a class="brand" href="#">6G1W</a>
+	  <ul class="nav">
+		<li class="active"><a href="index.html"><i class="fa fa-home fa-fw"></i> Home</a></li>
+		<li><a href="dictionary.html"><i class="fa fa-book fa-fw"></i> Dictionary</a></li>
+		<li><a href="tutorials.html"><i class="fa fa-pencil-square fa-fw"></i> Tutorials</a></li>
+		<li><a href="quizzes.html"><i class="fa fa-question fa-fw"></i> Quizzes</a></li>
+		<li><a href="about.html"><i class="fa fa-group fa-fw"></i> About</a></li>
+	  </ul>
+	</div>
+</div>
