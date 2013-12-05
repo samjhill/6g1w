25c49b8c86974f05c0a732d2ee72b627056ddf00
Sam Hill <samuhill@gmail.com>
2013-12-05 00:39:11 -0500

    ok, problem was the source. trying a new source

--- php/proxy.php
+++ php/proxy.php
@@ -3,7 +3,7 @@
 header('Content-type: application/xml');
 
 // Website url to open
-$daurl = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=rit483d65-f477-4935-ac6d-35e12287a5b&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=' . $_GET["search"];
+$daurl = 'http://www.cssheaven.com/feed/';
 
 // Get that website's content
 $handle = fopen($daurl, "r");
