31fdfd91fcbf70ee011028307f4030075689f15b
LiLiu <lxl3889@g.rit.edu>
2013-12-04 21:56:30 -0500

    widht-height quizzes php nav include

--- quizzes/width-height.php
+++ quizzes/width-height.php
@@ -1,3 +1,8 @@
+<?php
+
+  $currentPage="Quizzes";
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -44,18 +49,8 @@
     
 </head>
 <body>
-    <div class="navbar">
-		<div class="navbar-inner">
-		  <a class="brand" href="#">6G1W</a>
-		  <ul class="nav">
-			<li><a href="../index.html"><i class="fa fa-home fa-fw"></i> Home</a></li>
-			<li><a href="../dictionary.html"><i class="fa fa-book fa-fw"></i> Dictionary</a></li>
-			<li><a href="../tutorials.html"><i class="fa fa-pencil-square fa-fw"></i> Tutorials</a></li>
-			<li class="active"><a href="../quizzes.html"><i class="fa fa-question fa-fw"></i> Quizzes</a></li>
-			<li><a href="../about.html"><i class="fa fa-group fa-fw"></i> About</a></li>
-		  </ul>
-		</div>
-    </div>
+	<!--php navigation include-->
+	<?php include("../navigation.php"); ?>
     
     <div id="content">
         <form name ="quiz2" id="quiz2"
