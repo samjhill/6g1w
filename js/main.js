93be4a632c27a192aa824f06114b2b41fe2adb52
Sam Hill <samuhill@gmail.com>
2013-12-08 14:32:13 -0500

    add published date

--- js/main.js
+++ js/main.js
@@ -16,11 +16,13 @@ function parseXml(xml){
     $(xml).find("item").each(function() {
             
             console.log(this);
+            var date = $(this).find("pubDate").text();
             var title = $(this).find("title").text();
 	    var link =  $(this).find("link").text();
             
             
             $('#gallery').append('<h3>' + title + '</h3>');
+            $('#gallery').append('<p>' + date + '</p>');
             $('#gallery').append('<script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=http://www.cssheaven.com/glamour/"></script>');
             //$('#gallery').append('<script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=' + encodeURIComponent(link) +'"></script>');
 	    $('#gallery').append('<a href="' + link + '">' + link + '</a></div>');
