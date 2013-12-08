39fd487d8493b82a7c5d8b58482ce56afae063f2
Daniel Dorrity <djd6545@g.rit.edu>
2013-12-08 16:07:10 -0500

    maybe this will work? probably not

--- js/array.js
+++ js/array.js
@@ -1,9 +1,22 @@
-var array = ["Keyframes", "Animation", "AnimationName",
-"AnimationDuration", "AnimationTimingFunction", "AnimationDelay",
-"AnimationIterationCount", "OverflowX", "OverflowY", "ColorOpacity"
+var array = ["collapseKeyframes", "collapseAnimation", "collapseAnimationName",
+"collapseAnimationDuration", "collapseAnimationTimingFunction", "collapseAnimationDelay",
+"collapseAnimationIterationCount", "collapseOverflowX", "collapseOverflowY", "collapseColorOpacity"
 ];
 
 var rand = array[Math.floor(Math.random() * array.length)];
+
+if (rand == array[0] || rand == array[1] || rand == array[2] || rand == array[3] || rand == array[4] || rand = array[5]) || rand == array[6])
+{
+    rand = "animation";
+}
+else if(rand == array[7] || rand == array[8])
+{
+    rand = "box";
+}
+else if(rand == array[9])
+{
+    rand = "color";
+}
 
 document.getElementById("newLink1").href += rand;
 
