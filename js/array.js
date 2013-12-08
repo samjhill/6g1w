4698fa6b397d1fce91b86c4f8e9623cc73f68bfa
Daniel Dorrity <djd6545@g.rit.edu>
2013-12-08 15:26:02 -0500

    more testing

--- js/array.js
+++ js/array.js
@@ -1,16 +1,10 @@
-function setNew(field)
-{
-    var array = ["Keyframes", "Animation", "AnimationName",
-    "AnimationDuration", "AnimationTimingFunction", "AnimationDelay",
-    "AnimationIterationCount", "OverflowX", "OverflowY", "ColorOpacity"
-    ];
+var array = ["Keyframes", "Animation", "AnimationName",
+"AnimationDuration", "AnimationTimingFunction", "AnimationDelay",
+"AnimationIterationCount", "OverflowX", "OverflowY", "ColorOpacity"
+];
 
-    var rand = array[Math.floow(Math.random() * array.length)];
+var rand = array[Math.floow(Math.random() * array.length)];
 
-    var link = $('#newLink1');
-    link.attr('href', link.attr('href') + array[3]);
+document.getElementById("newLink1").href += rand;
 
-    var name = document.getElementById("newLinkId1");
-
-    name.innerHTML(rand);
-}
+console.log(rand);
