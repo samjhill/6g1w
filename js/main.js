05b974f28ae7908918233ec55bf30abe4f430a54
Sam Hill <samuhill@gmail.com>
2013-12-08 14:50:59 -0500

    encode uri again

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function parseXml(xml){
             var title = $(this).find("title").text();
 	    var link =  $(this).find("link").text();
             
-            $('#gallery').append('<div class="well"><h3>' + title + '</h3><p>' + date.substr(0,16) + '</p><script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=http://www.cssheaven.com/glamour/"></script><a href="' + link + '">' + link + '</a></div>');
+            $('#gallery').append('<div class="well"><h3>' + title + '</h3><p>' + date.substr(0,16) + '</p><script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=' + encodeURIComponent("http://www.cssheaven.com/glamour/") + '"></script><a href="' + link + '">' + link + '</a></div>');
            
     });
     
