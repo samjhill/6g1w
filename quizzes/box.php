1acc1b4dc437a638c0b74e3fe9c135a33473b8dd
Shane Date <shanedate@Shanes-MacBook-Pro.local>
2013-12-08 14:58:15 -0500

    q

--- quizzes/box.php
+++ quizzes/box.php
@@ -65,34 +65,34 @@
 	
 	<div class="question">
 	    <span>1)  What property specifies whether or not to clip the left/right edges of the element's content?</span>
-	    <label for ="q1a1"> <input type="radio" name="q1" value="correct" id="q1a1"/>     overflow-x; </label>
-	    <label for ="q1a2"> <input type="radio" name="q1" value="incorrect" id="q1a2"/>    overflow-y; </label>
+	    <label for ="q1a1"> <input type="radio" name="q1" value="correct" id="q1a1"/>a) overflow-x; </label>
+	    <label for ="q1a2"> <input type="radio" name="q1" value="incorrect" id="q1a2"/> b) overflow-y; </label>
 	</div>
 	
 	<div class="question">
 	    <span>2)  What property specifies whether or not to clip the top/bottom edges of the element's content?</span>
-	    <label for ="q2a1"><input type="radio" name="q2" value="incorrect" id="q2a1"/>     overflow-x; </label>
-	    <label for ="q2a2"><input type="radio" name="q2" value="correct" id="q2a2"/>     overflow-y; </label>
+	    <label for ="q2a1"><input type="radio" name="q2" value="incorrect" id="q2a1"/>a) overflow-x; </label>
+	    <label for ="q2a2"><input type="radio" name="q2" value="correct" id="q2a2"/>b) overflow-y; </label>
 	</div>
 	
 	<div class="question">
 	    <span>3) Rotation-point rotates an element around a given point</span>
-	    <label for ="q1a1"><input type="radio" name="q3" value="incorrect" id="q3a1"/>    true </label>
-	    <label for ="q3a2"><input type="radio" name="q3" value="correct" id="q3a2"/>    false </label>
+	    <label for ="q1a1"><input type="radio" name="q3" value="incorrect" id="q3a1"/>a) true </label>
+	    <label for ="q3a2"><input type="radio" name="q3" value="correct" id="q3a2"/>b) false </label>
 	</div>
 	
 	<div class="question">
 	    <span>4) What is the name of the property that sets the preferred scrolling method?</span>
-	    <label for ="q4a1"><input type="radio" name="q4" value="incorrect" id="q4a1"/>    overflow-scroll; </label>
-	    <label for ="q4a2"><input type="radio" name="q4" value="incorrect" id="q4a2"/>    overflow-method; </label>
-	    <label for ="q4a3"><input type="radio" name="q4" value="incorrect" id="q4a3"/>   overflow; </label>
-	    <label for ="q4a4"><input type="radio" name="q4" value="correct" id="q4a4"/>   overflow-style; </label>
+	    <label for ="q4a1"><input type="radio" name="q4" value="incorrect" id="q4a1"/>a) overflow-scroll; </label>
+	    <label for ="q4a2"><input type="radio" name="q4" value="incorrect" id="q4a2"/>b) overflow-method; </label>
+	    <label for ="q4a3"><input type="radio" name="q4" value="incorrect" id="q4a3"/>c) overflow; </label>
+	    <label for ="q4a4"><input type="radio" name="q4" value="correct" id="q4a4"/>d) overflow-style; </label>
 	</div>
 	
 	<div class="question">
 	    <span>5) Rotation-direction is a CSS property</span>
-	    <label for ="q5a1"><input type="radio" name="q5" value="incorrect" id="q5a1"/>    true</label>
-	    <label for ="q5a2"><input type="radio" name="q5" value="correct" id="q5a2"/>   false</label>
+	    <label for ="q5a1"><input type="radio" name="q5" value="incorrect" id="q5a1"/>a) true</label>
+	    <label for ="q5a2"><input type="radio" name="q5" value="correct" id="q5a2"/>b) false</label>
 	</div>
 	
 	<button type="button" class="btn btn-primary" onclick="validate() ;">Grade My Exam</button>
