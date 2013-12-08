d23332537430fe43c35b6dcc9e46e96492b34b98
Sam Hill <samuhill@gmail.com>
2013-12-08 14:35:41 -0500

    substring on date

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function parseXml(xml){
             
             
             $('#gallery').append('<h3>' + title + '</h3>');
-            $('#gallery').append('<p>' + date + '</p>');
+            $('#gallery').append('<p>' + date.substr(0,14) + '</p>');
             $('#gallery').append('<script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=http://www.cssheaven.com/glamour/"></script>');
             //$('#gallery').append('<script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=' + encodeURIComponent(link) +'"></script>');
 	    $('#gallery').append('<a href="' + link + '">' + link + '</a></div>');
