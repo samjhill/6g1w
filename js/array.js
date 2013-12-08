13cd944141c97cf607eb164d78392b70de333253
Daniel Dorrity <djd6545@g.rit.edu>
2013-12-08 15:29:47 -0500

    stupid w

--- js/array.js
+++ js/array.js
@@ -3,7 +3,7 @@ var array = ["Keyframes", "Animation", "AnimationName",
 "AnimationIterationCount", "OverflowX", "OverflowY", "ColorOpacity"
 ];
 
-var rand = array[Math.floow(Math.random() * array.length)];
+var rand = array[Math.floor(Math.random() * array.length)];
 
 document.getElementById("newLink1").href += rand;
 
