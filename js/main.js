9707801b5d254bbf969e97bf40bb929782224ed9
Sam Hill <samuhill@gmail.com>
2013-12-08 14:42:51 -0500

    display:inline

--- js/main.js
+++ js/main.js
@@ -20,10 +20,8 @@ function parseXml(xml){
             var title = $(this).find("title").text();
 	    var link =  $(this).find("link").text();
             
-            $('#gallery').append('<div style="display:inline;"> <h3>' + title + '</h3><p>' + date.substr(0,14) + '</p><script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=http://www.cssheaven.com/glamour/"></script><a href="' + link + '">' + link + '</a></div>');
-            //$('#gallery').append('<script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=' + encodeURIComponent(link) +'"></script>');
-	    
-            
+            $('#gallery').append('<div style="display:inline;"> <h3 style="display:inline">' + title + '</h3><p>' + date.substr(0,14) + '</p><script type="text/javascript" src="http://grabz.it/services/javascript.ashx?key=M2M1MDljMzhkNmJmNGQ5M2FiYzU4MDNhMjJkZWJkMjQ=&url=http://www.cssheaven.com/glamour/"></script><a href="' + link + '">' + link + '</a></div>');
+           
     });
     
 }
