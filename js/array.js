dfdb98db75fe494d4b01dc57672f5858a3892456
NinjaGezus <djd6545@g.rit.edu>
2013-12-09 12:34:32 -0500

    name fix

--- js/array.js
+++ js/array.js
@@ -7,68 +7,57 @@ var rand = array[Math.floor(Math.random() * array.length)];
 
 console.log(rand);
 
-if (rand == "collapseKeyframes" || rand == "collapseAnimation" || rand == "collapseAnimationName"
-    || rand == "collapseAnimationDuration" || rand == "collapseAnimationTimingFunction" || rand == "collapseAnimationDelay"
-    || rand == "collapseAnimationIterationCount")
-{
-    var name = "animation";
-    $('#newLink1').html("Animation");
-}
-else if (rand == "collapseOverflowX" || rand == "collapseOverflowY")
-{
-    var name = "box-properties";
-    $('#newLink1').html("Overflow");
-}
-else if (rand == "collapseColorOpacity")
-{
-    var name = "color";
-    $('#newLink1').html("Color Opacity");
-}
-
 if(rand == "collapseKeyframes")
 {
 	var link = "animation";
+	$('#newLink1').html("@keyframes");
 }
 else if(rand == "collapseAnimation")
 {
 	var link = "collapseKeyframes";
+	$('#newLink1').html("Animation");
 }
 else if(rand == "collapseAnimationName")
 {
 	var link = "collapseAnimation";
+	$('#newLink1').html("Animation Name");
 }
 else if(rand == "collapseAnimationDuration")
 {
 	var link = "collapseAnimationName";
+	$('#newLink1').html("Animation Duration");
 }
 else if(rand == "collapseAnimationTimingFunction")
 {
 	var link = "collapseAnimationDuration";
+	$('#newLink1').html("Animation Timing Function");
 }
 else if(rand == "collapseAnimationDelay")
 {
 	var link = "collapseAnimationTimingFunction";
+	$('#newLink1').html("Animation Delay");
 }
 else if(rand == "collapseAnimationIterationCount")
 {
 	var link = "collapseAnimationDelay";
+	$('#newLink1').html("Animation Iteration Count");
 }
 else if(rand == "collapseOverflowX")
 {
 	var link = "box-properties";
+	$('#newLink1').html("Overflow-X");
 }
 else if(rand == "collapseOverflowY")
 {
 	var link = "collapseOverflowX";
+	$('#newLink1').html("Overflow-Y");
 }
 else if(rand == "collapseColorOpacity")
 {
 	var link = "collapseColor";
+	$('#newLink1').html("Color Opacity");
 }
-
-
 
 console.log(name);
 
-//document.getElementById('newLink1').href += "dictionary.php#" + rand;
 $('#newLink1').attr('href', 'dictionary.php#' + link);
